550ca64cc636a1f0a4451962d5e14a14aa166899
Ian Engelbrecht <ianicus.za@gmail.com>
2020-10-22 16:20:00 +0200

    updated to correct API endpoint

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('isomorphic-fetch')
 const API_KEY = require('./API_KEY')
 
 //currently just Harpactirinae //TODO update to call any taxon
-const tpediaURL = `http://www.tarantupedia.com/api/get/zoo/occurances?app=1&api_key=${API_KEY}&type=subfamily&id=5`
+const tpediaURL = `http://www.tarantupedia.com/api/get/zoo/occurrences?app=1&api_key=${API_KEY}&type=subfamily&id=5`
 
 
 const filePath = String.raw`C:\Users\engelbrechti\Google Drive\Baboon spider atlas\Data`
@@ -18,7 +18,7 @@ fetch(tpediaURL).then(async res => {
   let records = json.results
   let mappedRecords = []
   for (let record of records) {
-    let decimalCoordinates = record.latlng
+    let decimalCoordinates = record.decimalCoordinates
     let decimalLatitude, decimalLongitude
     if(decimalCoordinates && decimalCoordinates.trim()){
       if(decimalCoordinates.includes(',')){
@@ -33,16 +33,17 @@ fetch(tpediaURL).then(async res => {
     let eventDate = [record.year, record.month, record.day].filter(x=>x).join('-')
 
     let mappedRecord = {
-      datasetID: record.id,
-      catalogNumber: record.name,
-      originalNameUsage: record.original_taxon, //very bad, this is actually scientific but this hack is needed to match other datasets
-      scientificName: record.current_taxon,
-      typeStatus: record.is_type == 'yes' ? 'type' : null, 
-      sex: record.gender, 
+      datasetID: record.databaseRecordId,
+      catalogNumber: record.catalogNumber,
+      originalNameUsage: record.originalNameUsage, //very bad, this is actually scientific but this hack is needed to match other datasets
+      scientificName: record.scientificName,
+      typeStatus: record.isType == 'yes' ? 'type' : null, 
+      sex: record.sex, 
+      locality: record.locality,
       decimalCoordinates, 
       decimalLatitude,
       decimalLongitude,
-      recordedBy: record.collector,
+      recordedBy: record.recordedBy,
       year: record.year,
       month: record.month, 
       day: record.day,
